dc9133a2682b345dc272905b1b2fae2285fde76d
Eddy Zhu <eddygithub@gmail.com>
2015-01-24 17:27:33 -0500

    completed homework assignment #2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## create a matrix and cache the matrix's value
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	inverseVal <<- NULL
+	set <- function(y){
+		x <- y
+		m <<- NULL
+	}
+	get <- function() x
+	setInverse <- function(val) inverseVal <<- val
+	getInverse <- function() inverseVal
+	list(set=set, get=get, setInverse=setInverse, getInverse=getInverse) 
 }
 
 
-## Write a short comment describing this function
-
+## retrieve the cached inverse of matrix X, or recompute the inverse value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+	inverseVal <- x$getInverse()
+	if(!is.null(inverseVal)){
+		message("getting cached inverse value")
+		return(inverseVal)
+	}
+	
+	data <- x$get()
+	inverseVal <- solve(data) 
+	x$setInverse(inverseVal)
+	inverseVal
 }
